f59e9b7f26b0d21a125d0dd814f19ee2a93cb642
Aniket Chaudhri <79798301+AniketChaudhri@users.noreply.github.com>
2023-01-09 22:59:21 +0530

    lab1

---
+++ Lab1/matrixmain.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+#include "matrixmul.hpp"
+
+using namespace std;
+
+#define n 600
+
+int main()
+{
+    
+    vector<vector<int>> A(n, vector<int>(n));
+    vector<vector<int>> B(n, vector<int>(n));
+    vector<vector<int>> C(n, vector<int>(n));
+
+    // Read matrix A and B from matrix1.txt and matrix2.txt
+
+    freopen("matrix1.txt","r",stdin);
+       
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            cin>>A[i][j];
+        }
+    }
+    for (auto x:A){
+        for (auto y:x){
+            cout << y << " ";
+        }
+    }
+    freopen("matrix2.txt","r",stdin);
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            cin>>B[i][j];
+        }
+    }
+    for (auto x:B){
+        for (auto y:x){
+            cout << y << " ";
+        }
+    }
+    matrixmul(A,B,C);
+    freopen("matrix3.txt","w",stdout);
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            cout<<C[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
---
+++ Lab1/matrixmul.hpp
@@ -0,0 +1,16 @@
+// c++ library to multiply two matrices
+
+#ifndef MATRIXMUL_HPP
+#define MATRIXMUL_HPP
+
+#include <iostream>
+#include <vector>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+// matrix multiplication function
+void matrixmul(vector<vector<int>>, vector<vector<int>>, vector<vector<int>>);
+
+#endif
+
